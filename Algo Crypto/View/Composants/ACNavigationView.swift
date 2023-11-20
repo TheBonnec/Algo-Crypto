bb19460ae5226cd77f4df2e717d9860809fb0866
TheBonnec <thomaslebonnec@icloud.com>
2023-11-19 22:55:38 -0500

    1.0 partie 1

---
+++ Algo Crypto/View/Composants/ACNavigationView.swift
@@ -0,0 +1,18 @@
+//
+//  ACNavigationView.swift
+//  Algo Crypto
+//
+//  Created by Thomas Le Bonnec on 17/11/2023.
+//
+
+import SwiftUI
+
+struct ACNavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ACNavigationView()
+}
---
+++ Algo Crypto/View/Composants/SavedItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  SavedItemCell.swift
+//  Algo Crypto
+//
+//  Created by Thomas Le Bonnec on 19/11/2023.
+//
+
+import SwiftUI
+
+struct SavedItemCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedItemCell()
+}
